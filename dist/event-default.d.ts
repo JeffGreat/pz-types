6ebce7a4da36da9601529ba7c9c867e48389b0f0
JeffGreat <jf.gruet@gmail.com>
2019-11-07 19:08:11 +0100

    add default data for event

--- dist/event-default.d.ts
+++ dist/event-default.d.ts
@@ -1,3 +1,44 @@
 import { EventSettings, SettingsBadge } from './event';
 export declare const defaultEventSettings: EventSettings;
 export declare const defaultEventSettingsBadge: SettingsBadge;
+export declare const defaultEventStats: {
+    contacts: {
+        total: number;
+        source: {
+            import: number;
+            subscription: number;
+            coOpt: number;
+            checkin: number;
+            added: number;
+        };
+    };
+};
+export declare const defaultTheme: {
+    primary: string;
+    secondary: string;
+    light: string;
+    dark: string;
+    background: string;
+};
+export declare const defaultSettingsAccess: {
+    participant: {
+        enabled: boolean;
+        password: string;
+    };
+    conferencier: {
+        enabled: boolean;
+        password: string;
+    };
+    moderateur: {
+        enabled: boolean;
+        password: string;
+    };
+    emargeur: {
+        enabled: boolean;
+        password: string;
+    };
+    commercial: {
+        enabled: boolean;
+        password: string;
+    };
+};
--- dist/event-default.js
+++ dist/event-default.js
@@ -9,3 +9,29 @@ exports.defaultEventSettingsBadge = {
         errorCorrectionLevel: 'L',
     },
 };
+exports.defaultEventStats = {
+    contacts: {
+        total: 0,
+        source: {
+            import: 0,
+            subscription: 0,
+            coOpt: 0,
+            checkin: 0,
+            added: 0,
+        },
+    },
+};
+exports.defaultTheme = {
+    primary: '',
+    secondary: '',
+    light: '',
+    dark: '',
+    background: '',
+};
+exports.defaultSettingsAccess = {
+    participant: { enabled: false, password: '' },
+    conferencier: { enabled: false, password: '' },
+    moderateur: { enabled: false, password: '' },
+    emargeur: { enabled: false, password: '' },
+    commercial: { enabled: false, password: '' },
+};
