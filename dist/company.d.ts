9c04b312a4e864372d5272a193dac1279c4fb15e
JeffGreat <jf.gruet@gmail.com>
2019-11-16 23:24:55 +0100

    update routersettings

--- dist/company.d.ts
+++ dist/company.d.ts
@@ -1,10 +1,28 @@
+export interface SmsRouter {
+    sender: string;
+    provider?: {
+        endpoint: string;
+        key: string;
+        secret: string;
+        consummerKey: string;
+    };
+}
+export interface EmailRouter {
+    sender: string;
+    provider?: {
+        endpoint: string;
+        key: string;
+        secret: string;
+        consummerKey: string;
+    };
+}
 export interface Company {
     id: string;
     name: string;
     logo: number;
     routingSettings: {
-        sms: Object;
-        email: Object;
+        sms: SmsRouter;
+        email: EmailRouter;
     };
     creditsSms: number;
     creditsEmail: number;
--- src/company.ts
+++ src/company.ts
@@ -1,8 +1,18 @@
+export interface SmsRouter {
+    sender: string;
+    provider?: {endpoint: string; key: string; secret: string; consummerKey: string};
+}
+
+export interface EmailRouter {
+    sender: string;
+    provider?: {endpoint: string; key: string; secret: string; consummerKey: string};
+}
+
 export interface Company {
     id: string;
     name: string;
     logo: number;
-    routingSettings: {sms: Object; email: Object};
+    routingSettings: {sms: SmsRouter; email: EmailRouter};
     creditsSms: number;
     creditsEmail: number;
 }
