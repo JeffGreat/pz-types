cf3c57466147e3b12169d37609f867df67064408
JeffGreat <jf.gruet@gmail.com>
2019-12-16 11:20:46 +0100

    add default formfields

--- src/subscription-form-default.ts
+++ src/subscription-form-default.ts
@@ -2,5 +2,11 @@ export const defaultSubscriptionForm = {
     status: 0,
     open: true,
     quota: -1,
-    content: undefined,
+    content: {
+        width: 100,
+        twoColumns: false,
+        showLabel: true,
+        inlineFields: false,
+        formFields: [],
+    },
 };
